0cfc58ac73ae955151a9067c7b06cb95ea802526
Raiden <raiden12121@gmail.com>
2021-08-26 23:36:51 -0700

    added greet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # import "packages" from flask
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # create a Flask instance
 app = Flask(__name__)
@@ -10,7 +10,15 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-
+@app.route('/greet', methods=['GET', 'POST'])
+def greet():
+    # submit button has been pushed
+    if request.form:
+        name = request.form.get("name")
+        if len(name) != 0:  # input field has content
+            return render_template("greet.html", nickname=name)
+    # starting and empty input default
+    return render_template("greet.html", nickname="World")
 # connects /kangaroos path to render kangaroos.html
 @app.route('/kangaroos/')
 def kangaroos():
@@ -30,7 +38,6 @@ def hawkers():
 @app.route('/stub/')
 def stub():
     return render_template("stub.html")
-
 
 # runs the application on the development server
 if __name__ == "__main__":
