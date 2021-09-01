8b64f8f361391793ac4b17c97f16d12a2a0bdbbb
briantang <brian.g.tang.135@gmail.com>
2021-09-01 14:41:48 -0700

    for testing purposes

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/greet', methods=['GET', 'POST'])
-def greet():
-    # submit button has been pushed
-    if request.form:
-        name = request.form.get("name")
-        if len(name) != 0:  # input field has content
-            return render_template("greet.html", nickname=name)
-    # starting and empty input default
-    return render_template("greet.html", nickname="World")
+# @app.route('/greet', methods=['GET', 'POST'])
+# def greet():
+#     # submit button has been pushed
+#     if request.form:
+#         name = request.form.get("name")
+#         if len(name) != 0:  # input field has content
+#             return render_template("greet.html", nickname=name)
+#     # starting and empty input default
+#     return render_template("greet.html", nickname="World")
 
 
 # connects /kangaroos path to render kangaroos.html
@@ -46,6 +46,17 @@ def stub():
 @app.route('/raiden/')
 def raiden():
     return render_template("raiden.html")
+
+
+@app.route('/brian', methods=['GET', 'POST'])
+def brian():
+    # submit button has been pushed
+    if request.form:
+        name = request.form.get("name")
+        if len(name) != 0:  # input field has content
+            return render_template("brian.html", nickname=name)
+    # starting and empty input default
+    return render_template("brian.html", nickname="World")
 
 
 # runs the application on the development server
