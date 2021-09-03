d3ff96e0b873f365bcedffe3035709e03974c41c
Raiden <raiden12121@gmail.com>
2021-09-03 15:01:52 -0700

    added and fixed navbar

--- main.py
+++ main.py
@@ -10,18 +10,6 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-
-# @app.route('/greet', methods=['GET', 'POST'])
-# def greet():
-#     # submit button has been pushed
-#     if request.form:
-#         name = request.form.get("name")
-#         if len(name) != 0:  # input field has content
-#             return render_template("greet.html", nickname=name)
-#     # starting and empty input default
-#     return render_template("greet.html", nickname="World")
-
-
 # connects /kangaroos path to render kangaroos.html
 @app.route('/kangaroos/')
 def kangaroos():
